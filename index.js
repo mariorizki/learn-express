6731e36031749a8f7f144997288d008c66b33bc0
Mario Rizki <mariorizki07@gmail.com>
2019-02-19 15:33:21 +0700

    response send object

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Hello World!'
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
