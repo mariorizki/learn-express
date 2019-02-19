dee287f640dd018213efd83e6d0efff2bad50d37
Mario Rizki <mariorizki07@gmail.com>
2019-02-19 15:44:46 +0700

    implement /users

--- index.js
+++ index.js
@@ -2,9 +2,43 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const users = [
+  {
+    id: 1,
+    name: 'Mario'
+  },
+  {
+    id: 2,
+    name: 'Fahri'
+  },
+  {
+    id: 3,
+    name: 'Sakti'
+  },
+  {
+    id: 4,
+    name: 'Ajin'
+  },
+  {
+    id: 5,
+    name: 'Bara'
+  },
+  {
+    id: 6,
+    name: 'Jo'
+  }
+];
+
 app.get('/', (req, res) => {
   res.send({
     message: 'Hello World!'
+  });
+});
+
+app.get('/users', (req, res) => {
+  res.send({
+    message: 'List of Users',
+    data: users
   });
 });
 
